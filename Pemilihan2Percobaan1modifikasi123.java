bb92e85626049e4149b821b37ce145b4b0a82eab
fri17da <syafridaadeauliahakim@gmail.com>
2024-10-04 10:01:13 +0700

    commit

--- Pemilihan2Percobaan1modifikasi123.java
+++ Pemilihan2Percobaan1modifikasi123.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Pemilihan2Percobaan123 {
+public class Pemilihan2Percobaan1modifikasi123 {
     public static void main(String[] args) {
         Scanner input23 = new Scanner(System.in);
 
@@ -8,8 +8,7 @@ public class Pemilihan2Percobaan123 {
          System.out.print("Masukkan Tahun: ");
          tahun = input23.nextInt();
 
-        if ((tahun % 4) == 0) {
-            if ((tahun % 100) != 0);
+        if ((tahun % 4 == 0 && tahun % 100 != 0 ) || (tahun % 400 == 0)) {
             System.out.println("Tahun Kabisat");
         } else {
            System.out.println("Bukan Tahun Kabisat");
