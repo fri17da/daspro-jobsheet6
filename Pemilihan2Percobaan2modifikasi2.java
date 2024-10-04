c4cee544217bfa8a75bc69b07088bca6cccc83b4
fri17da <syafridaadeauliahakim@gmail.com>
2024-10-04 23:59:21 +0700

    commit

--- Pemilihan2Percobaan2modifikasi2.java
+++ Pemilihan2Percobaan2modifikasi2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Pemilihan2Percobaan223 {
+public class Pemilihan2Percobaan2modifikasi2 {
     public static void main(String[] args) {
         Scanner input23 = new Scanner(System.in);
 
