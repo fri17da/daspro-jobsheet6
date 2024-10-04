f5f993af166cb79b0ee2c8e8bdaec488cf4dddf7
fri17da <syafridaadeauliahakim@gmail.com>
2024-10-04 14:47:00 +0700

    commit

--- Pemilihan2Tugas123.java
+++ Pemilihan2Tugas123.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
-public class Pemilihan2Tugas123 {
-    public static void main (String [] args) {
-        Scanner sc = new Scanner(System.in);
 
+public class Pemilihan2Tugas123 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
         int bil1 = 28, bil2 = 54, bil3 = 15;
         int max;
 
         if (bil1 >= bil2) {
             max = bil1;
-            if (bil1 >= bil3) { 
-                max = bil1;
-            } else if (bil2 >= bil1) {
-                max = bil2;
-                if (bil2 >= bil3) {
-                    max = bil2;
-            } else {
-                max = bil3;
-            }  
-        }    
-
-    }
+        } else {
+            max = bil2;
+        if (bil2 >= bil3){
+            max = bil2;
+        } else {
+            max = bil3;
+        }
+        System.out.println("Bilangan terbesar: " + max);
+        }         
 }
+}   
+ 
